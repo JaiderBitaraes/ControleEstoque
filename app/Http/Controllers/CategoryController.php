074baad34cfe81afb7564b085987bdc329b377bc
jaider@master <jaiderbitaraes@gmail.com>
2017-04-14 15:43:01 -0300

    Adicionado rota create em Categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,10 +19,23 @@ class CategoryController extends Controller
         return Response::json($data);
 
     }
-    public function get(){
+    public function getAll(){
 
-        $get = CategoryModel::find(1);
-        return Response::json($get);
+        $data = CategoryModel::all();
+        return Response::json($data);
 
+    }
+    public function getById($id){
+        $data = CategoryModel::find($id);
+        return Response::json($data);
+    }
+    public function update($id){
+        $data = Input::all();
+        $result = CategoryModel::where('id',$id) ->update($data);
+        return Response::json($data);
+    }
+    public function delete($id){
+        $result = CategoryModel::where('id',$id) ->delete();
+        return Response::json($result);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::get('/home', 'HomeController@index');
 
 Route::post('/api/category/create', 'CategoryController@create');
 
-Route::get('/api/category/get', 'CategoryController@get');
+Route::get('/api/category/getAll', 'CategoryController@getAll');
 
+Route::get('/api/category/getById/{id}','CategoryController@getById');
+
+Route::put('/api/category/update/{id}','CategoryController@update');
+
+Route::delete('/api/category/delete/{id}','CategoryController@delete');
 // Route::post();
